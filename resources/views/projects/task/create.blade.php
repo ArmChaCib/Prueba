7e0fe6fb494a3447ef43bbfdacbbd51e1cc744cb
ArmChaCib <arm_chiva@hotmail.com>
2016-12-16 00:33:05 -0600

    Validaciones
    Embellecer las validaciones de la creción de tareas

--- resources/views/projects/task/create.blade.php
+++ resources/views/projects/task/create.blade.php
@@ -5,30 +5,50 @@
 
   <form class="" action="/projects/show/{{$idProject}}/tasks/create" method="post">
     <input type="hidden" value="{{Auth::user()->id}}" name="user_id">
+
     <input type="hidden" value="{{$idProject}}" name="project_id">
-    <div class="form-group">
+
+    <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
       <label>Nombre de la tarea</label>
       <input type="text" name="title" class="form-control" placeholder="Nombre de la tarea">
-      {{ ($errors->has('title')) ? $errors->first('title') : '' }} <br>
+      @if ($errors->has('title'))
+          <span class="help-block">
+              <strong>{{ $errors->first('title') }}</strong>
+          </span>
+      @endif
     </div>
 
-    <div class="form-group">
+    <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
       <label>Descripción de la tarea</label>
       <textarea  class="form-control" name="description" rows="3" placeholder="Descripción"></textarea>
-      {{ ($errors->has('description')) ? $errors->first('description') : '' }} <br>
+       @if ($errors->has('description'))
+          <span class="help-block">
+              <strong>{{ $errors->first('description') }}</strong>
+          </span>
+      @endif
     </div>
 
-    <div class="form-group">
+    <div class="form-group{{ $errors->has('start') ? ' has-error' : '' }}">
       <div class="col-xs-6">
         <label>Fecha de inicio</label>
         <input type="date" name="start">
-        {{ ($errors->has('start')) ? $errors->first('start') : '' }}
+         @if ($errors->has('start'))
+          <span class="help-block">
+              <strong>{{ $errors->first('start') }}</strong>
+          </span>
+      @endif
       </div>
+    </div>
 
+    <div class="form-group{{ $errors->has('end') ? ' has-error' : '' }}">
       <div class="col-xs-6">
         <label>Fecha de terminación</label>
       <input type="date" name="end">
-      {{ ($errors->has('end')) ? $errors->first('end') : '' }}
+      @if ($errors->has('end'))
+          <span class="help-block">
+              <strong>{{ $errors->first('end') }}</strong>
+          </span>
+      @endif
       </div>
     </div>
 
